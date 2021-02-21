66e58ee49144699e6599db4fda0a851ee83d3d99
LeeBongHyeon <11jjklol@naver.com>
2021-02-22 01:50:34 +0900

    playerAdd

--- player.js
+++ player.js
@@ -1,24 +1,24 @@
 
-let playerPos= new [2];//플레이어의 중점
+let playerPos= [];//플레이어의 중점
 let r = 20;//캐릭터는 반지름이 20px인 원으로 그려진다.
 
-const __player = {};
+//const __player = {};
 
-__player.playerStart=function()
+function playerStart()
 {
-    playerPos[0]=400;
-    playerPos[1]=250;
+    playerPos[0]=250;
+    playerPos[1]=400;
 }
 
-__player.playerUp=function()//위쪽 방향키 눌렀을 때
+function playerUp()//위쪽 방향키 눌렀을 때
 {
     playerPos[1]+=1;
 }
-__player.playerDown=function()//아무 조작도 없을때 플레이어는 떨어진다.
+function playerDown()//아무 조작도 없을때 플레이어는 떨어진다.
 {
     playerPos[1]-=1;
 }
-__player.IsGameOver=function()//플레이어가 화면의 바닥이나 천장에 닿았을때 false반환
+function IsGameOver()//플레이어가 화면의 바닥이나 천장에 닿았을때 false반환
 {
     if(playerPos[1]-r<=0)
         return false;
@@ -27,11 +27,11 @@ __player.IsGameOver=function()//플레이어가 화면의 바닥이나 천장에
 
         return true;
 }
-__player.GetplayerPos=function()//플레이어 위치 반환
+function GetplayerPos()//플레이어 위치 반환
 {
     return playerPos;
 }
-__player.GetRadius=function()//플레이어의 반지름 반환
+function GetRadius()//플레이어의 반지름 반환
 {
     return r;
 }
