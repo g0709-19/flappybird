005a43d13dd8e1e3514150456f1a5a326afeb949
LeeBongHyeon <11jjklol@naver.com>
2021-02-22 00:38:55 +0900

    test

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 const testm = require('./testm');
 
 testm.hello();
+
+function draw(){
+    var canvas = document.getElementById('tutorial');
+    if (canvas.getContext){
+      var ctx = canvas.getContext('2d');
+    }
+  }
